e6925b44d251b34959c1293119a3c4bcc11521e8
leemin <leemin119@gmail.com>
2018-03-29 13:38:03 +0900

    .

--- my-project/src/components/Main.vue
+++ my-project/src/components/Main.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
+  <!--<div>
     <ul class="nav nav-pills " id="pills-tab" role="tablist">
       <li class="nav-item">
-        <a class="nav-link active" id="pills-about-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">ABOUT</a>
+        <a class="nav-link active" id="pills-about-tab" data-toggle="pill" href="#pills-about" role="tab" aria-controls="pills-home" aria-selected="true">ABOUT</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" id="pills-company-tab" data-toggle="pill" href="#pills-company" role="tab" aria-controls="pills-profile" aria-selected="false">COMPANY</a>
@@ -28,6 +28,155 @@
       <div class="tab-pane fade" id="pills-courses" role="tabpanel" aria-labelledby="pills-courses-tab">courses</div>
       <div class="tab-pane fade" id="pills-contacts" role="tabpanel" aria-labelledby="pills-contacts-tab">contacts</div>
     </div>
+  </div>-->
+  <div>
+  <div class="body1">
+    <div class="main">
+      <!-- header -->
+        <div class="wrapper">
+          <nav>
+            <ul id="menu">
+              <li><a href="index.html"><cufon class="cufon cufon-canvas" alt="About" style="width: 51px; height: 15px;"><canvas width="58" height="18" style="width: 58px; height: 18px; top: -1px; left: 0px;"></canvas>About</cufon></a></li>
+              <li><a href="company.html"><cufon class="cufon cufon-canvas" alt="Company" style="width: 78px; height: 15px;"><canvas width="83" height="18" style="width: 83px; height: 18px; top: -1px; left: 0px;"></canvas>Company</cufon></a></li>
+              <li><a href="partners.html"><cufon class="cufon cufon-canvas" alt="Partner" style="width: 68px; height: 15px;"><canvas width="74" height="18" style="width: 74px; height: 18px; top: -1px; left: 0px;"></canvas>Partner</cufon></a></li>
+              <li><a href="business.html"><cufon class="cufon cufon-canvas" alt="Business" style="width: 70px; height: 15px;"><canvas width="77" height="18" style="width: 77px; height: 18px; top: -1px; left: 0px;"></canvas>Business</cufon></a></li>
+              <li><a href="courses.html"><cufon class="cufon cufon-canvas" alt="Courses" style="width: 68px; height: 15px;"><canvas width="75" height="18" style="width: 75px; height: 18px; top: -1px; left: 0px;"></canvas>Courses</cufon></a></li>
+              <li class="end"><a href="contacts.html"><cufon class="cufon cufon-canvas" alt="Contacts" style="width: 79px; height: 15px;"><canvas width="86" height="18" style="width: 86px; height: 18px; top: -1px; left: 0px;"></canvas>Contacts</cufon></a></li>
+            </ul>
+          </nav>
+          <ul id="icons">
+            <li><a href="https://docs.google.com/forms/d/13aiW7y0vYi2t6JjLrNsGZhmMrPSIR2R0mSQwYya-hfs/viewform" target="_blank"><img src="images/survey.png" alt=""></a></li>
+          </ul>
+        </div>
+        <div class="wrapper">
+          <h1>
+            <a href="index.html" id="logo">Learn Center</a>
+          </h1>
+        </div>
+        <div id=""><canvas width="113" height="55" style="width: 113px; height: 55px; top: -4px; left: -1px;"></canvas>We Will Open The World</div>
+      <!-- / header -->
+    </div>
+  </div>
+  <div class="body2">
+    <div class="main">
+      <!-- content -->
+      <section id="content">
+        <!-- 				<div class="wrapper"> -->
+        <!-- 					<div class="pad1 pad_top1"> -->
+        <!-- 						<article class="cols marg_right1"> -->
+        <!-- 							<figure> -->
+        <!-- 								<a href="#"><img src="images/page1_img1.jpg" alt=""></a> -->
+        <!-- 							</figure> -->
+        <!-- 							<span class="font1">Our Mission Statement</span> -->
+        <!-- 						</article> -->
+        <!-- 						<article class="cols marg_right1"> -->
+        <!-- 							<figure> -->
+        <!-- 								<a href="#"><img src="images/page1_img2.jpg" alt=""></a> -->
+        <!-- 							</figure> -->
+        <!-- 							<span class="font1">Performance Report</span> -->
+        <!-- 						</article> -->
+        <!-- 						<article class="cols"> -->
+        <!-- 							<figure> -->
+        <!-- 								<a href="#"><img src="images/page1_img3.jpg" alt=""></a> -->
+        <!-- 							</figure> -->
+        <!-- 							<span class="font1">Prospective Employer</span> -->
+        <!-- 						</article> -->
+        <!-- 					</div> -->
+        <!-- 				</div> -->
+        <div class="box1">
+          <div class="wrapper">
+            <article class="col1">
+              <div class="pad_left1">
+                <h2><cufon class="cufon cufon-canvas" alt="Welcome " style="width: 155px; height: 32px;"><canvas width="180" height="39" style="width: 180px; height: 39px; top: -6px; left: -1px;"></canvas>Welcome </cufon><cufon class="cufon cufon-canvas" alt="to " style="width: 48px; height: 32px;"><canvas width="74" height="39" style="width: 74px; height: 39px; top: -6px; left: -1px;"></canvas>to </cufon><cufon class="cufon cufon-canvas" alt="Jaen " style="width: 74px; height: 32px;"><canvas width="99" height="39" style="width: 99px; height: 39px; top: -6px; left: -1px;"></canvas>Jaen </cufon><cufon class="cufon cufon-canvas" alt="Center" style="width: 106px; height: 32px;"><canvas width="124" height="39" style="width: 124px; height: 39px; top: -6px; left: -1px;"></canvas>Center</cufon></h2>
+                <p class="font2"><cufon class="cufon cufon-canvas" alt="Jaen " style="width: 40px; height: 22px;"><canvas width="58" height="27" style="width: 58px; height: 27px; top: -4px; left: -1px;"></canvas>Jaen </cufon><cufon class="cufon cufon-canvas" alt="is " style="width: 18px; height: 22px;"><canvas width="36" height="27" style="width: 36px; height: 27px; top: -4px; left: -1px;"></canvas>is </cufon><cufon class="cufon cufon-canvas" alt="an " style="width: 26px; height: 22px;"><canvas width="44" height="27" style="width: 44px; height: 27px; top: -4px; left: -1px;"></canvas>an </cufon><cufon class="cufon cufon-canvas" alt="expert " style="width: 59px; height: 22px;"><canvas width="77" height="27" style="width: 77px; height: 27px; top: -4px; left: -1px;"></canvas>expert </cufon><cufon class="cufon cufon-canvas" alt="group " style="width: 56px; height: 22px;"><canvas width="73" height="27" style="width: 73px; height: 27px; top: -4px; left: -1px;"></canvas>group </cufon><cufon class="cufon cufon-canvas" alt="for " style="width: 30px; height: 22px;"><canvas width="48" height="27" style="width: 48px; height: 27px; top: -4px; left: -1px;"></canvas>for </cufon><cufon class="cufon cufon-canvas" alt="Korea " style="width: 53px; height: 22px;"><canvas width="71" height="27" style="width: 71px; height: 27px; top: -4px; left: -1px;"></canvas>Korea </cufon><cufon class="cufon cufon-canvas" alt="IT..." style="width: 29px; height: 22px;"><canvas width="48" height="27" style="width: 48px; height: 27px; top: -4px; left: -1px;"></canvas>IT...</cufon></p>
+                <p>
+                  자앤은 <strong>Java</strong>를 기반으로 하는 다양한 영역에 축적된 지식과 경험으로 컨설팅,
+                  프로토타이핑, 교육을 전문으로 하는 기업입니다. 기업체 위탁교육 및 IT 교육 사업 및 컨설팅으로, 귀사의
+                  기술력과 경쟁력을 향상 시키는데 가장 좋은 선택이 될 것입니다. 자바와 안드로이드 관련된 교육은 자앤(Jaen)에
+                  맡겨 주십시오.
+                </p>
+                <!--               <a href="courses.html">Courses</a>, <a href="business.html">Business</a>, <a href="partners.html">Partners</a>, <a href="contacts.html">Contacts</a></p> -->
+                <!--             </div> -->
+                <!--             <a href="#" class="button"><span><span>Read More</span></span></a> -->
+              </div>
+              <div class="pad_left1">
+                <h2><cufon class="cufon cufon-canvas" alt="Approach " style="width: 158px; height: 32px;"><canvas width="184" height="39" style="width: 184px; height: 39px; top: -6px; left: -1px;"></canvas>Approach </cufon><cufon class="cufon cufon-canvas" alt="to " style="width: 48px; height: 32px;"><canvas width="74" height="39" style="width: 74px; height: 39px; top: -6px; left: -1px;"></canvas>to </cufon><cufon class="cufon cufon-canvas" alt="IT " style="width: 34px; height: 32px;"><canvas width="59" height="39" style="width: 59px; height: 39px; top: -6px; left: -1px;"></canvas>IT </cufon><cufon class="cufon cufon-canvas" alt="Education!" style="width: 171px; height: 32px;"><canvas width="198" height="39" style="width: 198px; height: 39px; top: -6px; left: -1px;"></canvas>Education!</cufon></h2>
+              </div>
+              <div class="wrapper">
+                <figure class="left marg_right1">
+                  <img src="http://www.androidjava.com/images/page1_img4.jpg" alt="">
+                </figure>
+                <p class="pad_bot1 pad_top2">세계적으로 임베디드 SW사업의 급성장과 더불어 임베디드
+                  SW는 개인용 컴퓨터와는 달리 특정한 요구사항을 가지고 있으며, 미리 정의된 작업(Task)만을 수행하도록
+                  ①시스템을 동작시키는 소프트웨어를 ②하드웨어에 내장하여 ③특수한 기능만을 가진 시스템으로써, 현재 적용되고 있는
+                  기술 영역으로써는 Smart TV, BDP(Blue-ray Disc Player), STB(Settop Box),
+                  전기차(EV), BMS(Battery Management System), 모바일(안드로이드, 아이폰), 스마트
+                  교통 단말기등의 사업분야에 적용되고 있습니다. 점점 치열해지고 있는 기존IT시장의 한계를 극복하고, 글로벌
+                  리더십을 확보하기 위해 새로운 성장 동력 발굴에 힘쓰고 있으며, 그 중 하나가 성장사업인 모바일 서비스 사업,
+                  꿈의 공장을 구현하는 제조/설비 엔지니어링 사업, 지능형 인프라인 스마트 그린 시티, 스마트 교통 및 스마트
+                  그리드 사업 등 성장 잠재력이 큰 신 성장 사업에 대규모 확장에 대비한 IT 기반 기술 교육을 Jaen이 담당하고
+                  있습니다.</p>
+              </div>
+              <div class="pad_top2">
+                <a href="courses.html" class="button"><span><span>Read
+											More</span></span></a>
+              </div>
+            </article>
+            <article class="col2 pad_left2">
+              <div class="pad_left1">
+                <h2><cufon class="cufon cufon-canvas" alt="New " style="width: 77px; height: 32px;"><canvas width="102" height="39" style="width: 102px; height: 39px; top: -6px; left: -1px;"></canvas>New </cufon><cufon class="cufon cufon-canvas" alt="Programs" style="width: 154px; height: 32px;"><canvas width="172" height="39" style="width: 172px; height: 39px; top: -6px; left: -1px;"></canvas>Programs</cufon></h2>
+              </div>
+              <ul class="list1">
+                <li><a href="courses.html">Hybrid Mobile Web App</a></li>
+                <li><a href="courses.html">Android &amp; NFC technology</a></li>
+                <li><a href="courses.html">SQLite development</a></li>
+                <li><a href="courses.html">Android network technology</a></li>
+                <li><a href="courses.html">Analysis &amp; Design for
+                  Embeded</a></li>
+              </ul>
+              <div class="pad_left1">
+                <h2><cufon class="cufon cufon-canvas" alt="Latest " style="width: 110px; height: 32px;"><canvas width="136" height="39" style="width: 136px; height: 39px; top: -6px; left: -1px;"></canvas>Latest </cufon><cufon class="cufon cufon-canvas" alt="News" style="width: 84px; height: 32px;"><canvas width="102" height="39" style="width: 102px; height: 39px; top: -6px; left: -1px;"></canvas>News</cufon></h2>
+              </div>
+              <div class="wrapper">
+                <span class="date"><cufon class="cufon cufon-canvas" alt="27" style="width: 36px; height: 34px;"><canvas width="53" height="41" style="width: 53px; height: 41px; top: -3px; left: -1px;"></canvas>27</cufon></span>
+                <p class="pad_top2">
+                  <a href="http://developer.android.com/about/versions/jelly-bean.html">Oct, 2014</a><br>Android 5.0 Lolly pop!...
+                </p>
+              </div>
+              <div class="wrapper">
+                <span class="date"><cufon class="cufon cufon-canvas" alt="1" style="width: 11px; height: 34px;"><canvas width="35" height="41" style="width: 35px; height: 41px; top: -3px; left: -1px;"></canvas>1</cufon></span>
+                <p class="pad_top2">
+                  <a href="http://www.oracle.com/technetwork/java/javase/overview/java8-2100321.html">Jun, 2014</a><br>Java 8 is a revolutionary release of the world’s #1 development platform ...
+                </p>
+              </div>
+              <div class="pad_top2">
+                <a href="http://developer.android.com/develop/index.html" class="button"><span><span>Read More</span></span></a>
+              </div>
+            </article>
+          </div>
+        </div>
+      </section>
+      <!-- content -->
+      <!-- footer -->
+      <footer>
+        <div class="wrapper">
+          <article class="call">
+            <span class="call1"><cufon class="cufon cufon-canvas" alt="Call " style="width: 58px; height: 32px;"><canvas width="83" height="39" style="width: 83px; height: 39px; top: -6px; left: -1px;"></canvas>Call </cufon><cufon class="cufon cufon-canvas" alt="Us " style="width: 40px; height: 32px;"><canvas width="65" height="39" style="width: 65px; height: 39px; top: -6px; left: -1px;"></canvas>Us </cufon><cufon class="cufon cufon-canvas" alt="Now: " style="width: 74px; height: 32px;"><canvas width="99" height="39" style="width: 99px; height: 39px; top: -6px; left: -1px;"></canvas>Now: </cufon></span><span class="call2"><cufon class="cufon cufon-canvas" alt="02.588.8929" style="width: 177px; height: 31px;"><canvas width="189" height="37" style="width: 189px; height: 37px; top: -3px; left: -1px;"></canvas>02.588.8929</cufon></span>
+            <span class="pad_top2 font3 pad_left3"> <a href="https://docs.google.com/forms/d/13aiW7y0vYi2t6JjLrNsGZhmMrPSIR2R0mSQwYya-hfs/viewform" class="button" target="_blank"><span><span> Go
+										to Survey </span></span></a>
+						</span>
+          </article>
+          <article class="col_4 pad_left2">
+            Copyright © <a href="#">Jaen co.,Ltd</a> All Rights Reserved<br>
+            Design by <a target="_blank" href="http://www.androidjava.com/">androidjava.com</a>
+          </article>
+        </div>
+      </footer>
+      <!-- / footer -->
+    </div>
+  </div>
+  <div align="center">137-822 Korea, Seoul, Seocho-Gu,
+    Bangbae-Dong,Leadaersvill 104 @jaen</div>
   </div>
 </template>
 
@@ -38,7 +187,10 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
-  }
+  },
+  created: function () {
+    Cufon.now();
+  },
 }
 </script>
 
